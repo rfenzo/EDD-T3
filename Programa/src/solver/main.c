572909f2ff384c1306e49f2914326c673bc67123
antonio <alopez7@uc.cl>
2018-06-03 22:21:08 -0400

    Listo

---
+++ Programa/src/solver/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "random.h"
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		printf("Modo de uso: ./solver test.txt\n");
+		return 0;
+	}
+
+	// Codigo
+
+	return 0;
+}
